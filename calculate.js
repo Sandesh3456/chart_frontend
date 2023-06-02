3f69118805598d7870e5a5358c5f1ab6445769b0
Sandesh <pathak2057@gmail.com>
2023-06-02 12:39:28 +0545

    Console elements removed

--- calculate.js
+++ calculate.js
@@ -1,5 +1,4 @@
 import myJson from './data.json' assert {type: 'json'};
-console.log(myJson);
 var barColors = ["hsl(10, 79%, 65%)", "hsl(10, 79%, 65%)","hsl(186, 34%, 60%)","hsl(10, 79%, 65%)","hsl(10, 79%, 65%)","hsl(10, 79%, 65%)","hsl(10, 79%, 65%)"];
 new Chart(
     document.getElementById('chart_1'),
@@ -21,12 +20,9 @@ let sum = (prev, cur) => {
   }
 // const arr= JSON.parse(myJson);
 // const arrSum = arr.map(row => row.marks);
-console.log("abc")
 const arrSum = myJson.map(row => row.amount);
 const result = arrSum.reduce(sum, 0);
-console.log(arrSum);
 document.getElementById("month_amount").innerHTML=result;
-        
 
 
 
