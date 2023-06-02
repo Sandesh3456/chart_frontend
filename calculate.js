59647d0a752a6e41783cf53d42d51d9d2f5b3f0c
Sandesh <pathak2057@gmail.com>
2023-06-02 10:19:36 +0545

    Design Changes

--- calculate.js
+++ calculate.js
@@ -1,9 +1,6 @@
-// import Chart from 'chart.js/auto'
-
-
 import myJson from './data.json' assert {type: 'json'};
 console.log(myJson);
-
+var barColors = ["hsl(10, 79%, 65%)", "hsl(10, 79%, 65%)","hsl(186, 34%, 60%)","hsl(10, 79%, 65%)","hsl(10, 79%, 65%)","hsl(10, 79%, 65%)","hsl(10, 79%, 65%)"];
 new Chart(
     document.getElementById('chart_1'),
     {
@@ -12,6 +9,7 @@ new Chart(
         labels: myJson.map(row => row.day),
         datasets: [
             {
+            backgroundColor: barColors,
             data: myJson.map(row => row.amount),
             }
         ]
